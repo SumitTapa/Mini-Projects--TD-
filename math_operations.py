d83c19a829f35fa9e5f3b7b64b24b3c1389c3a30
SumitTapa <164578930+SumitTapa@users.noreply.github.com>
2025-06-25 12:06:59 +0530

    Delete math_operations.py

--- math_operations.py
+++
@@ -1,7 +0,0 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-print(f"Addition: {num1 + num2}\n")
-print(f"Subtraction: {num1 - num2}\n")
-print(f"Multiplication: {num1 * num2}\n")
-print(f"Division: {num1 / num2}")
