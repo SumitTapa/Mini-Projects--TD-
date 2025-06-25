2dcf024ca721cf606da5a27b43a6158db1bf20d2
SumitTapa <164578930+SumitTapa@users.noreply.github.com>
2025-06-25 12:05:14 +0530

    Add files via upload

---
+++ ASSIGNMENT 2:/even_odd_problem.py
@@ -0,0 +1,5 @@
+_number=int(input("enter a number you wish:"))
+if _number%2==0:
+    print(_number,"is an even number")
+else :
+    print(_number, "is an odd number")
---
+++ ASSIGNMENT 2:/sum_of_integers.py
@@ -0,0 +1,4 @@
+_total_sum=0
+for i in range (0,51):
+    _total_sum+= i
+print('The sum of numbers from 1 t0 50 is',_total_sum)
