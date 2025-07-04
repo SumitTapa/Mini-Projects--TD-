13e42c8b84e884891721edf0b0ad968f609d34f5
SumitTapa <164578930+SumitTapa@users.noreply.github.com>
2025-07-04 16:24:00 +0530

    Add files via upload

---
+++ ASSIGNMENT 5/List_Slicing.py
@@ -0,0 +1,6 @@
+original_list=[3, 7, 1, 9, 4, 2, 6, 5, 8, 10]
+sliced_list=original_list[0:5]
+reversed_sliced_list=sliced_list[::-1]
+print("The original list is", original_list)
+print("Extracted list of first five elements", sliced_list)
+print("Reversed list is", reversed_sliced_list)
